4e494ed1bc80c24ce5f9654f69a951dc39a877a5
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2023-06-13 16:25:06 +0800

    id and classes

--- client/src/pages/AppForm1.js
+++ client/src/pages/AppForm1.js
@@ -1,44 +1,55 @@
 import { Link } from "react-router-dom";
 import "../styles/base.css";
-import "../styles/appform1.css"
+import "../styles/appform1.css";
 
 function AppForm1() {
-    return (
-        <div id="container">
-            <h1>Application</h1>
-            <hr/>
+  return (
+    <div id="container">
+      <h1>Application</h1>
+      <hr />
 
-            <div id="child">
-                <form id="application">
-                    <div>
-                        <label for="region">Region</label>
-                   
-                        <select name="region" id="region" class="dropdown">
-                            <option>NCR</option>
-                            <option>CALABARZON</option>
-                            <option>MIMAROPA</option>
-                        </select>
-                  
-                        <label for="chapter">Chapter</label>
-                    
-                        <select name="chapter" id="chapter" class="dropdown">
-                            <option>Chapter One</option>
-                            <option>Chapter Two</option>
-                            <option>Chapter Three</option>
-                        </select>
-                    </div>
+      <div id="child">
+        <form id="application">
+          <div className="grid">
+            <div className="itemone">
+              <label for="region" className="labels">
+                Region
+              </label>
 
-                    <Link to="/appform2">
-                    <button type="button" form="next" id="primary-btn" value="Next">
-						Next 
-                    </button>
-                    </Link>
-
-
-                </form>
+              <select name="region" id="region" className="dropdown">
+                <option>NCR</option>
+                <option>CALABARZON</option>
+                <option>MIMAROPA</option>
+              </select>
             </div>
-        </div>
-    )
+
+            <div class="itemtwo">
+              <label for="chapter" className="labels">
+                Chapter
+              </label>
+
+              <select name="chapter" id="chapter" className="dropdown">
+                <option>Chapter One</option>
+                <option>Chapter Two</option>
+                <option>Chapter Three</option>
+              </select>
+            </div>
+          </div>
+
+          <Link to="/appform2">
+            <button
+              type="button"
+              form="next"
+              className="primary-btn"
+              value="Next"
+            >
+              Next
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AppForm1;
