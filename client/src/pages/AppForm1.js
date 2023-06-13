b3269492a27a0b41491e3d88741155c4b267065f
yanajuson <103924317+yanajuson@users.noreply.github.com>
2023-06-13 16:35:01 +0800

    Revert "started application form pages"
    This reverts commit 9a5176bdccb138d71a9d26e2f8671af21756b525.

--- client/src/pages/AppForm1.js
+++ client/src/pages/AppForm1.js
@@ -1,55 +1,11 @@
-import { Link } from "react-router-dom";
 import "../styles/base.css";
-import "../styles/appform1.css";
 
 function AppForm1() {
-  return (
-    <div id="container">
-      <h1>Application</h1>
-      <hr />
-
-      <div id="child">
-        <form id="application">
-          <div className="grid">
-            <div className="itemone">
-              <label for="region" className="labels">
-                Region
-              </label>
-
-              <select name="region" id="region" className="dropdown">
-                <option>NCR</option>
-                <option>CALABARZON</option>
-                <option>MIMAROPA</option>
-              </select>
-            </div>
-
-            <div class="itemtwo">
-              <label for="chapter" className="labels">
-                Chapter
-              </label>
-
-              <select name="chapter" id="chapter" className="dropdown">
-                <option>Chapter One</option>
-                <option>Chapter Two</option>
-                <option>Chapter Three</option>
-              </select>
-            </div>
-          </div>
-
-          <Link to="/appform2">
-            <button
-              type="button"
-              form="next"
-              className="primary-btn"
-              value="Next"
-            >
-              Next
-            </button>
-          </Link>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <p>This is the first page of the application form.</p>
+        </div>
+    )
 }
 
 export default AppForm1;
