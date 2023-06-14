9fd1e932e84b5362463172823881d1afec8330a2
yanajuson <103924317+yanajuson@users.noreply.github.com>
2023-06-15 00:46:52 +0800

    application pages layouts - finished
    TO-DO:
    - fix and improve layouts
    - proceed to next screens (user logged in, viewing application details, etc.)

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
-import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import AppForm1 from './pages/AppForm1';
-import AppForm2 from './pages/AppForm2';
-import AppForm3 from './pages/AppForm3'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import AppForm1 from "./pages/AppForm1";
+import AppForm2 from "./pages/AppForm2";
+import AppForm3 from "./pages/AppForm3";
+import AppForm4 from "./pages/AppForm4";
+import AppForm5 from "./pages/AppForm5";
 
-
-import Header from './components/Header'
+import Header from "./components/Header";
 function App() {
   return (
     <>
-    <Header/>
-    <Routes>
-           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element = {<Contact/>} />
-          <Route path="appform1" element ={<AppForm1/>}/>
-          <Route path="appform2" element ={<AppForm2/>}/>
-          <Route path="appform3" element ={<AppForm3/>}/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="appform1" element={<AppForm1 />} />
+        <Route path="appform2" element={<AppForm2 />} />
+        <Route path="appform3" element={<AppForm3 />} />
+        <Route path="appform4" element={<AppForm4 />} />
+        <Route path="appform5" element={<AppForm5 />} />
       </Routes>
     </>
   );
---
+++ client/src/pages/AppForm5.js
@@ -0,0 +1,75 @@
+import { Link } from "react-router-dom";
+import "../styles/base.css";
+import "../styles/appform5.css";
+
+function AppForm5() {
+  return (
+    <div className="container container-fluid ">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Application Submitted!</h1>
+        </div>
+      </div>
+
+      <hr />
+      <div className="row">
+        <div className="col-md-12 conf-message">
+          Your application has been submitted! <br />
+          Please use your assigned Applicant ID below, and set a password for
+          your login credentials to see your application results once available.
+        </div>
+      </div>
+
+      <form className="g-2">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="row mb-3">
+              <label
+                for="inputEmail"
+                className="col-md-4 col-form-label text-right"
+              >
+                Applicant ID
+              </label>
+              <input
+                class="form-control"
+                id="disabledInput"
+                type="text"
+                placeholder="Disabled input here..."
+                disabled
+              />{" "}
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="row mb-3">
+              <label
+                for="inputEmail"
+                className="col-md-4 col-form-label text-right"
+              >
+                Set Password
+              </label>
+              <input
+                class="form-control"
+                id="disabledInput"
+                type="text"
+                placeholder="Password"
+              />{" "}
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 sub-btn">
+          <Link to="/">
+            <button type="submit" className="btn btn-primary">
+              SUBMIT
+            </button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AppForm5;
