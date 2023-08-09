d38ab950058d2defffaa24577b5173d929c75e1c
p-tolentino <95534051+p-tolentino@users.noreply.github.com>
2023-08-10 00:06:43 +0800

    award updates
    create award, apply award, submit application, approve application

--- client/src/pages/Events/EventsInstalling.jsx
+++ client/src/pages/Events/EventsInstalling.jsx
@@ -6,166 +6,140 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function EventsInstalling() {
-   return (
-      <div className="container">
-         <br />
-         <nav class="eventsNavbar">
-            <table class="navbar-table">
-               <tbody>
-                  <tr class="navbar-row">
-                     <td><a href="/eventsAttendance">Attendance</a></td>
-                     <td><a href="/eventsAthletic">Athletic</a></td>
-                     <td><a href="/eventsCivicService">Civic Service</a></td>
-                     <td><a href="/eventsConclave">Conclave</a></td>
-                     <td><a href="/eventsFineArts">Fine Arts</a></td>
-                     <td><a href="/eventsFundRaising">Fund Raising</a></td>
-                     <td><a href="/eventsInstalling">Installing</a></td>
-                     <td><a href="/eventsJournalism">Journalism</a></td>
-                  </tr>
-                  <tr className="navbar-row">
-                     <td><a href="/eventsMasonicAttendance">Masonic Attendance</a></td>
-                     <td><a href="/eventsMasonicService">Masonic Service</a></td>
-                     <td><a href="/eventsMerit">Merit</a></td>
-                     <td><a href="/eventsPetitions">Petitions</a></td>
-                     <td><a href="/eventsReligion">Religion</a></td>
-                     <td><a href="/eventsRitual">Ritual</a></td>
-                     <td><a href="/eventsScholastics">Scholastics</a></td>
-                     <td><a href="/eventsVisitation">Visitation</a></td>
-                  </tr>
-               </tbody>
-            </table>
-         </nav>
-         <br />
-         {/* Header */}
-         <div className="d-flex justify-content-between align-items-center mb-3">
-            <h1>Add Event - Installing </h1>
-            <Link to="/eventsInstalling">
-               <button type="submit" form="submit" className="primary-btn" value="ADD NEW ENTRY">
-                  ADD NEW ENTRY
-               </button>
-            </Link>
-         </div>
-         {/* Instruction */}
-         <div>
-            <p className="instruction"> Please take note that you can only submit the form if you have completed all the requirements.</p>
-         </div>
-         <hr />
-
-         <div className="row">
-            {/* First Column */}
-            <div className="col-md-4">
-               <table class="legend-table">
-                  <tr>
-                     <td>Date:</td>
-                     <td>date when the Installation is conducted</td>
-                  </tr>
-                  <tr>
-                     <td>Chapter:</td>
-                     <td>chapter that conducted the Installation</td>
-                  </tr>
-                  <tr>
-                     <td>Position:</td>
-                     <td>speaking part ONLY performs by the applicant</td>
-                  </tr>
-                  <tr>
-                     <td>Performance:</td>
-                     <td>base on the delivery of the Ritual part</td>
-                  </tr>
-               </table>
-            </div>
-            {/* Second Column */}
-            <div className="col-md-4">
-               <div className="row align-items-center mt-3">
-                  <div className="col-md-4">
-                     <label
-                        htmlFor="date"
-                        className="col-form-label text-right">
-                        Date:
-                     </label>
-                  </div>
-                  <div className="col-md-7">
-                     <input type="text" className="form-control" id="date" placeholder="MM/DD/YYYY" />
-                  </div>
-               </div>
-               <br />
-               <div className="row align-items-center mt-3">
-                  <div className="col-md-4">
-                     <label
-                        htmlFor="chapter"
-                        className="col-form-label text-left">
-                        Chapter:
-                     </label>
-                  </div>
-                  <div className="col-md-7">
-                     <select className="form-select form-control" id="chapter">
-                        <option>Chapter 1</option>
-                        <option>Chapter 2</option>
-                        <option>Chapter 3</option>
-                     </select>
-                  </div>
-               </div>
-               <br />
-               <div className="row align-items-center mt-3">
-                  <div className="col-md-4">
-                     <label
-                        htmlFor="position"
-                        className="col-form-label text-right">
-                        Position:
-                     </label>
-                  </div>
-                  <div className="col-md-7">
-                     <input type="text" className="form-control" id="position" placeholder="Enter Position" />
-                  </div>
-               </div>
-               <br />
-               <div className="row align-items-center mt-3">
-                  <div className="col-md-4">
-                     <label htmlFor="position" className="col-form-label text-right">
-                        Performance:
-                     </label>
-                  </div>
-                  <div className="col-md-7">
-                     <select className="form-select form-control" id="performance">
-                        <option>Beginner</option>
-                        <option>Amateur</option>
-                        <option>Expert</option>
-                        <option>Master</option>
-                     </select>
-                  </div>
-               </div>
-            </div>
-            {/* Third Column */}
-            <div className="col-md-4">
-               <div className="row align-items-center mt-3">
-                  <div className="col-md-3">
-                     <label
-                        htmlFor="uploadID"
-                        className="col-form-label text-right"
-                     >
-                        Proof:
-                     </label>
-                  </div>
-                  <div className="col-md-7">
-                     <input type="file" className="form-control" id="uploadID" />
-                  </div>
-               </div>
-            </div>
-            {/* Buttons */}
-            <div className="d-flex justify-content-between mt-4">
-               <Link to="/eventsHome">
-                  <button type="button" className="primary-btn">
-                     BACK
-                  </button>
-               </Link>
-               <Link to="/turnoverTF3">
-                  <button type="button" className="primary-btn" value="SUBMIT">
-                     SUBMIT
-                  </button>
-               </Link>
-            </div>
-         </div>
+  return (
+    <div className="container">
+      <br />
+      <br />
+      {/* Header */}
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <h1>Merit Bar Application - Installing </h1>
       </div>
-   );
+      {/* Instruction */}
+      <div>
+        <p className="instruction">
+          {" "}
+          Please take note that you can only submit the form if you have
+          completed all the requirements.
+        </p>
+      </div>
+      <hr />
+
+      <div className="row">
+        {/* First Column */}
+        <div className="col-md-4">
+          <table class="legend-table">
+            <tr>
+              <td>Date:</td>
+              <td>date when the Installation is conducted</td>
+            </tr>
+            <tr>
+              <td>Chapter:</td>
+              <td>chapter that conducted the Installation</td>
+            </tr>
+            <tr>
+              <td>Position:</td>
+              <td>speaking part ONLY performs by the applicant</td>
+            </tr>
+            <tr>
+              <td>Performance:</td>
+              <td>base on the delivery of the Ritual part</td>
+            </tr>
+          </table>
+        </div>
+        {/* Second Column */}
+        <div className="col-md-4">
+          <div className="row align-items-center mt-3">
+            <div className="col-md-4">
+              <label htmlFor="date" className="col-form-label text-right">
+                Date:
+              </label>
+            </div>
+            <div className="col-md-7">
+              <input
+                type="text"
+                className="form-control"
+                id="date"
+                placeholder="MM/DD/YYYY"
+              />
+            </div>
+          </div>
+          <br />
+          <div className="row align-items-center mt-3">
+            <div className="col-md-4">
+              <label htmlFor="chapter" className="col-form-label text-left">
+                Chapter:
+              </label>
+            </div>
+            <div className="col-md-7">
+              <select className="form-select form-control" id="chapter">
+                <option>Chapter 1</option>
+                <option>Chapter 2</option>
+                <option>Chapter 3</option>
+              </select>
+            </div>
+          </div>
+          <br />
+          <div className="row align-items-center mt-3">
+            <div className="col-md-4">
+              <label htmlFor="position" className="col-form-label text-right">
+                Position:
+              </label>
+            </div>
+            <div className="col-md-7">
+              <input
+                type="text"
+                className="form-control"
+                id="position"
+                placeholder="Enter Position"
+              />
+            </div>
+          </div>
+          <br />
+          <div className="row align-items-center mt-3">
+            <div className="col-md-4">
+              <label htmlFor="position" className="col-form-label text-right">
+                Performance:
+              </label>
+            </div>
+            <div className="col-md-7">
+              <select className="form-select form-control" id="performance">
+                <option>Beginner</option>
+                <option>Amateur</option>
+                <option>Expert</option>
+                <option>Master</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        {/* Third Column */}
+        <div className="col-md-4">
+          <div className="row align-items-center mt-3">
+            <div className="col-md-3">
+              <label htmlFor="uploadID" className="col-form-label text-right">
+                Proof:
+              </label>
+            </div>
+            <div className="col-md-7">
+              <input type="file" className="form-control" id="uploadID" />
+            </div>
+          </div>
+        </div>
+        {/* Buttons */}
+        <div className="d-flex justify-content-between mt-4">
+          <Link to="/eventsHome">
+            <button type="button" className="primary-btn">
+              BACK
+            </button>
+          </Link>
+          <Link to="/turnoverTF3">
+            <button type="button" className="primary-btn" value="SUBMIT">
+              SUBMIT
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EventsInstalling;
--- client/src/pages/Events/EventsValidation-Advisor.jsx
+++ client/src/pages/Events/EventsValidation-Advisor.jsx
@@ -35,13 +35,18 @@ function EventsValidation() {
       <div className="container">
          <br/>
 
+<<<<<<< Updated upstream
          {/* Header */}
+=======
+  const [selectedApplication, setSelectedApplication] = useState({});
+>>>>>>> Stashed changes
 
          <div className="d-flex justify-content-between align-items-center mb-3">
             <h1> Request for Merit Bars </h1>
          </div>
          <hr/>
 
+<<<<<<< Updated upstream
          <div className="row">
 
             {/* First Column */}
@@ -63,6 +68,23 @@ function EventsValidation() {
             </div>
 
             {/* Vertical Line */}
+=======
+  const [meritBarCtg, setCategory] = useState("");
+
+  // const handleNavigation = (action) => {
+  //   if (action === "prev") {
+  //     setCurrentIndex((prevIndex) =>
+  //       prevIndex === 0 ? prevIndex : prevIndex - 1
+  //     );
+  //   } else if (action === "next") {
+  //     setCurrentIndex((prevIndex) =>
+  //       prevIndex === currentList.length - 1 ? prevIndex : prevIndex + 1
+  //     );
+  //   }
+
+  //   setCurrentDisplay(currentList[currentIndex]);
+  // };
+>>>>>>> Stashed changes
 
             <div className="col-md-1">
                <div className="vl"></div>
@@ -70,6 +92,7 @@ function EventsValidation() {
             
             {/* Second Column */}
 
+<<<<<<< Updated upstream
             <div className="col-md-5 mx-auto">
                <h2> Attendance </h2>
                <div className="row align-items-center mt-3">
@@ -176,6 +199,329 @@ function EventsValidation() {
          </div>         
       </div>
    );
+=======
+      setUserData({
+        ...userData,
+        userID: user.data._id,
+        name: user.data.givenName + " " + user.data.lastName,
+        chapterID: user.data.chapterId,
+      });
+
+      const events = await axios.get(
+        `http://localhost:5000/getEvents/${user.data.chapterId}`
+      );
+
+      axios
+        .get("http://localhost:5000/getAllAwardApplications")
+        .then((applications) => {
+          setApplicationData({
+            ...applicationData,
+            applications: applications.data,
+            filteredApplications: applications.data.filter((application) => {
+              if (
+                application.isSubmitted === true &&
+                application.isApproved !== true
+              ) {
+                return true;
+              } else {
+                return false;
+              }
+            }),
+          });
+        });
+      setEventsData(events.data);
+    }
+
+    fetchEventsData();
+  }, []);
+
+  const styles = {
+    maxWidth: "200px",
+    maxHeight: "200px",
+    objectFit: "contain",
+  };
+
+  // const onChangeMeritBar = (e) => {
+  //   setCategory({
+  //     ...meritBars,
+  //     meritBarCtg: e.target.value,
+  //   });
+
+  //   console.log(selectedApplication);
+  // };
+
+  const handleSubmit = () => {
+    const props = {
+      fieldToUpdate: "isApproved",
+      updateValue: true,
+    };
+
+    axios
+      .post(
+        `http://localhost:5000/updateAwardApplication/${selectedApplication._id}`,
+        props
+      )
+      .then((res) => {
+        alert(
+          `Approved application for ${selectedApplication.name} (${selectedApplication.type} - ${selectedApplication.color})`
+        );
+        console.log("Updated and submitted application: " + res.data);
+        window.location.reload();
+      });
+  };
+
+  return (
+    <div className="container">
+      <br />
+
+      {/* Header */}
+
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <h1> Request for Merit Bars </h1>
+        {/* <div className="row align-items-center mt-3">
+          <div className="col-md-4">
+            <label htmlFor="meritBar" className="col-form-label text-right">
+              Merit Bar:
+            </label>
+          </div>
+          <div className="col-md-7">
+            <select
+              className="form-select"
+              name="meritBar"
+              id="meritBar"
+              value={meritBars.meritBarCtg}
+              onChange={onChangeMeritBar}
+            >
+              <option value="" disabled={meritBars.meritBarCtg !== 0} hidden>
+                Select Merit Bar
+              </option>
+              {meritBars.meritBars.map(function (meritBar) {
+                return (
+                  <option value={meritBar.value} key={meritBar.key}>
+                    {" "}
+                    {meritBar.key}{" "}
+                  </option>
+                );
+              })}
+            </select>{" "}
+          </div>
+        </div> */}
+      </div>
+      <hr />
+
+      <div className="row">
+        {/* First Column */}
+
+        <div className="col-md-6">
+          <table className="table table-bordered-custom">
+            <thead className="thead-custom">
+              <tr>
+                <th>Name of Applicant</th>
+                <th>Category</th>
+                <th>Color</th>
+                <th>View Application</th>
+              </tr>
+            </thead>
+            <tbody>
+              {applicationData.filteredApplications.map(function (application) {
+                return (
+                  <tr key={application.applicantID}>
+                    <td>{application.name}</td>
+                    <td>{application.type}</td>
+                    <td>{application.color}</td>
+                    <td>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => {
+                          setSelectedApplication(application);
+                          setCategory(application.type);
+                        }}
+                      >
+                        {" "}
+                        View Application
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Vertical Line */}
+
+        <div className="col-md-1">
+          <div className="vl"></div>
+        </div>
+
+        {/* Second Column */}
+        <div className="col-md-5 mx-auto">
+          {meritBarCtg === "Attendance" && (
+            <>
+              {eventsData.attendanceEvents.map((event) => {
+                if (event._id === selectedApplication.attendance.eventID) {
+                  return (
+                    <div key={selectedApplication.attendance.eventID}>
+                      <h2> Attendance </h2>
+                      <div className="row align-items-center mt-3">
+                        <div className="col-md-4">
+                          <label
+                            htmlFor="term"
+                            className="col-form-label text-left"
+                          >
+                            Term:
+                          </label>
+                        </div>
+                        <div className="col-md-8">
+                          <input
+                            type="text"
+                            className="form-control readonly-input"
+                            value={event.term}
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                      <div className="row align-items-center mt-3">
+                        <div className="col-md-4">
+                          <label
+                            htmlFor="meetingname"
+                            className="col-form-label text-right"
+                          >
+                            Meeting Name:
+                          </label>
+                        </div>
+                        <div className="col-md-8">
+                          <input
+                            type="text"
+                            className="form-control readonly-input"
+                            value={event.meetingName}
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                      <div className="row align-items-center mt-3">
+                        <div className="col-md-4">
+                          <label
+                            htmlFor="meetingdate"
+                            className="col-form-label text-right"
+                          >
+                            Meeting Date:
+                          </label>
+                        </div>
+                        <div className="col-md-8">
+                          <input
+                            type="date"
+                            className="form-control readonly-input"
+                            value={event.meetingDate.split("T")[0]}
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+              <div className="row align-items-center mt-3">
+                <div className="col-md-4">
+                  <label
+                    htmlFor="position"
+                    className="col-form-label text-right"
+                  >
+                    Position:
+                  </label>
+                </div>
+                <div className="col-md-8">
+                  <input
+                    type="text"
+                    className="form-control readonly-input"
+                    value={selectedApplication.attendance.position || ""}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div className="row align-items-center mt-3">
+                <div className="col-md-4">
+                  <label
+                    htmlFor="position"
+                    className="col-form-label text-right"
+                  >
+                    Performance:
+                  </label>
+                </div>
+                <div className="col-md-8">
+                  <input
+                    type="text"
+                    className="form-control readonly-input"
+                    value={selectedApplication.attendance.performance || ""}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div className="row align-items-center mt-3">
+                <div className="col-md-4">
+                  <label htmlFor="proof" className="col-form-label text-right">
+                    Proof:
+                  </label>
+                </div>
+                <div className="col-md-8">
+                  {selectedApplication.attendance.proof ? (
+                    <img
+                      src={selectedApplication.attendance.proof}
+                      alt="img"
+                      style={styles}
+                    />
+                  ) : (
+                    <p></p>
+                  )}
+                </div>
+              </div>
+
+              {/* Buttons 
+
+              <div className="d-flex justify-content-center mt-4">
+                <div className="mr-2">
+                  <button
+                    type="button"
+                    form="button"
+                    className="primary-btn"
+                    value="PREVIOUS"
+                    onClick={() => handleNavigation("prev")}
+                  >
+                    PREVIOUS
+                  </button>
+                </div>
+                <div className="ms-2">
+                  <button
+                    type="button"
+                    form="button"
+                    className="primary-btn"
+                    value="NEXT"
+                    onClick={() => handleNavigation("next")}
+                  >
+                    NEXT
+                  </button>
+                </div>
+              </div>*/}
+              {selectedApplication.isApproved === false && (
+                <div className="d-flex justify-content-end ms-5 w-100">
+                  <button
+                    type="button"
+                    form="button"
+                    className="primary-btn"
+                    value="NEXT"
+                    onClick={handleSubmit}
+                  >
+                    APPROVE
+                  </button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default EventsValidation;
